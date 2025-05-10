5a76dac6d95598338b65a118bdc028ab88d4af3d
@amianGO <damiangaviria8@gmail.com>
2025-05-10 12:59:18 -0500

    Revision Controlador

--- Administration/src/main/java/com/example/Administration/Controllers/AuthController.java
+++ Administration/src/main/java/com/example/Administration/Controllers/AuthController.java
@@ -58,6 +58,7 @@ public class AuthController {                                                   
         
             return ResponseEntity.ok(response);
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
